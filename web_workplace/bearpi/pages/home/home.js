10045491442225a02f0bf5b97c51f2bd296e4615
Jessic13 <963820036@qq.com>
2022-07-01 20:04:47 +0800

    commit message

---
+++ web_workplace/bearpi/pages/home/home.js
@@ -0,0 +1,34 @@
+// pages/home/home.js
+const app = getApp()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+  goToPage1: function () {
+    wx.navigateTo({
+      url: './pages/camera/camera',
+    })
+  },
+
+  
+  
+
+})
+
---
+++ web_workplace/bearpi/pages/set/set.js
@@ -0,0 +1,34 @@
+// pages/set/set.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }, 
+  
+  data: {
+    currentDate: '12:00',
+    minHour: 9,
+    maxHour: 23
+  },
+
+ 
+
+
+  
+})
